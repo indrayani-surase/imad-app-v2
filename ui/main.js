bed1dcfca484badaad3c339a09a0cf235c1fdf56
Indrayani surase <GayatriBs1997@gmail.com>
2017-03-23 02:33:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,23 @@ button.onclick= function() {
   request.open("GET","http://indrayani-surase.imad.hasura-app.io/",true);
   request.send(null);
 };
+
+//submit name
+
+var nameInput=document.getElementById("name");
+var name = nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    
+    //make request to server
+    var names=[name1,name2,name3,name4];
+    var list="";
+    for(i=0;i<names.length;i++){
+        list=='<li>'+names[i]+'</li>';
+        
+    }
+    //capture list of name and render it
+    var ul=document.getElementById("namelist");
+    ul.innerHTML=list;
+    
+};
