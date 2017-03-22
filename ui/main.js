a172345f58973747cb1ec456bdd695a14ca97b65
Indrayani surase <GayatriBs1997@gmail.com>
2017-03-23 01:48:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,21 @@
 var button=document.getElementById("counter");
 
-var counter=0;
+
 button.onclick= function() {
-  
-  counter=counter+1;
-  var span=document.getElementById("count");
-  span.innerHTML=counter.toString();
+    //create request
+    
+    var request= new XMLhttprequest();
+    
+    request.onreadystatechange=function(){
+        if(request.readystate ==XMLhttprequest.DONE){
+            if(request.status==200){
+                var counter=request.responseText;
+                var span=document.getElementById("count");
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+  //make request
+  request.open("GET","http://indrayani-surase.imad.hasura-app.io/",true);
+  request.send(null);
 };
