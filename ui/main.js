51f0bf27d37e14d86a8338d64e19de70d02168e0
Indrayani surase <GayatriBs1997@gmail.com>
2017-03-23 00:15:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,13 @@ element.innerHTML="new value";
 
 var img=document.getElementById("madi");
 
-img.onclick=function(){
-  img.style.marginleft="100px" ; 
+var marginLeft=0;
+function moveRight() {
+  marginleft=marginLeft+10;
+  img.style.marginLeft=marginLeft + 'px';
+};
+
+img.onclick=function() {
+    var interval=setInterval(moveRight,100);
+  
 };
